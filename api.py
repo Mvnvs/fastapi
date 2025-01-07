5203709f9dd65d357dc576707180efa4b907e4b0
Emmanuel L <81372068+Mvnvs@users.noreply.github.com>
2025-01-07 21:26:05 +0100

    Update api.py

--- api.py
+++ api.py
@@ -56,3 +56,11 @@ async def generate_image(file: UploadFile):
 
     # Retourner l'image sous forme de réponse HTTP
     return StreamingResponse(buffer, media_type="image/png")
+
+# Ajoute ceci pour utiliser le port défini par Render
+if __name__ == "__main__":
+    import uvicorn
+    import os
+
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
